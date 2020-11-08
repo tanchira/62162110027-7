38659db101b95b00fad936e5a777c2e47699fc05
tanchira <57671539+tanchira@users.noreply.github.com>
2020-11-08 20:00:44 +0700

    Update Exercise.cs

--- Exercise.cs
+++ Exercise.cs
@@ -8,6 +8,22 @@ namespace _62162110027_7
 {
     internal abstract class Sports  //class กีฬาเก็บวิธีการออกกำลังกาย   แคลรอลี่ และ คำวนณไว้ 
     {
+        public void HowtoExercise()
+        {
+
+        }
+        private int calorie;
+        public int Calorie
+        {
+            get { return calorie; }
+            set { calorie = value; }
+        }
+        public void Calculat(int Hours)
+        {
+            int count;
+            count = Hours * Calorie;
+            Console.WriteLine(count + " Calorie");
+        }
 
     }
 
